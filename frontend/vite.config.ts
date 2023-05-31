b360f7184b198d164ab6ca4d0ede4054b5909edc
Devin Pangestu <devinpangestu2000@gmail.com>
2023-05-31 17:19:05 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://noteappapi.vercel.app/",
         changeOrigin: true,
         secure: false,
       },
