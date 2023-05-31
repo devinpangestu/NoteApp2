73bae9c9441db3be8765a2134746c2a50117fe84
Devin Pangestu <devinpangestu2000@gmail.com>
2023-05-31 19:24:25 +0700

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,13 @@ import mongoose from "mongoose";
 
 const port = env.PORT;
 
-mongoose.connect(env.MONGO_CONNECTION_STRING)
-    .then(() => {
-        console.log("Mongoose connected");
-        app.listen(port, () => {
-            console.log("Server running on port: " + port);
-        });
-    })
-    .catch(console.error);
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+      console.log(env.MONGO_CONNECTION_STRING);
+    });
+  })
+  .catch(console.error);
